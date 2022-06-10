cc38a01c1caa4f7974f6ee58e932461bd413ab8b
GRND0 <zefiro.turchetta@gmail.com>
2022-06-10 16:16:28 +0200

    aggiunto codice per il prelievo dei dati dal database e loro stampa in html, bozza aspetto. Milestone 1

--- Milestone 1/index.php
+++ Milestone 1/index.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Milestone 1</title>
 </head>
+
 <body>
-    
+
+
+    <?php include "database.php";  ?>
+
+    <main>
+        <div class="wrapper">
+            <section class="contenitore-brani">
+                <?php foreach ($database as $element) { ?>
+                    <div class="contenitore-card">
+                        <img src="<?php echo $element["poster"];  ?>" alt="">
+                        <h3> <?php echo $element["author"];  ?> </h3>
+                        <small><?php echo $element["author"];  ?></small>
+                        <small><?php echo $element["year"];  ?> </small>
+                    </div>
+                <?php  } ?>
+            </section>
+        </div>
+    </main>
+
+    <img src="" alt="">
+
 </body>
+
 </html>
